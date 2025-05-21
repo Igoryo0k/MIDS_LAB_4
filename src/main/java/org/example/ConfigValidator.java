bcd3a81b69b606dd2686e0244b073ab49d063190
Yevhenii Butkevych <143118683+deLordeee@users.noreply.github.com>
2025-05-21 21:31:18 +0300

    Add validate() function for validation of config in ConfigValidator.java

--- src/main/java/org/example/ConfigValidator.java
+++ src/main/java/org/example/ConfigValidator.java
@@ -5,6 +5,40 @@ package org.example;
 
 
 
-public class ConfigValidator {
+import java.util.Map;
 
+public class ConfigValidator {
+    public static boolean validate(Map<String, Object> config, Schema schema) {
+        for (Map.Entry<String, String> field : schema.getFields().entrySet()) {
+            String key = field.getKey();
+            String expectedType = field.getValue();
+
+            if (!config.containsKey(key)) {
+                System.out.println("Missing required field: " + key);
+                return false;
+            }
+
+            Object value = config.get(key);
+
+            switch (expectedType) {
+                case "string":
+                    if (!(value instanceof String)) {
+                        System.out.println("Field '" + key + "' should be a string.");
+                        return false;
+                    }
+                    break;
+                case "int":
+                    if (!(value instanceof Integer)) {
+                        System.out.println("Field '" + key + "' should be an integer.");
+                        return false;
+                    }
+                    break;
+                
+                default:
+                    System.out.println("Unknown expected type: " + expectedType);
+                    return false;
+            }
+        }
+        return true;
+    }
 }
