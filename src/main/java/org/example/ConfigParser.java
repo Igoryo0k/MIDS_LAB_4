4270c04083f6c9a4d374c5fe3fbd632abc7f9137
ferer00 <155906971+ferer00@users.noreply.github.com>
2025-05-21 21:27:29 +0300

    Update ConfigParser.java

--- src/main/java/org/example/ConfigParser.java
+++ src/main/java/org/example/ConfigParser.java
@@ -1,9 +1,13 @@
 package org.example;
 
-// ================= ConfigParser.java =================
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-
+import java.io.IOException;
+import java.util.Map;
 
 public class ConfigParser {
-
+    public static Map<String, Object> parse(String jsonContent) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(jsonContent, Map.class);
+    }
 }
