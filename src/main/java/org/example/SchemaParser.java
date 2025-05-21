c54a1803cc3ba2f267bbd92e38be2b3f67156161
Yevhenii Butkevych <143118683+deLordeee@users.noreply.github.com>
2025-05-21 21:16:54 +0300

    Added parse() function with mapper in schema.java

--- src/main/java/org/example/SchemaParser.java
+++ src/main/java/org/example/SchemaParser.java
@@ -3,6 +3,16 @@ package org.example;
 // ================= SchemaParser.java =================
 
 
-public class SchemaParser {
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
+import java.util.Map;
+
+public class SchemaParser {
+    public static Schema parse(File jsonContent) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, String> fields = mapper.readValue(jsonContent, new TypeReference<>() {});
+        return new Schema(fields);
+    }
 }
