403741d43e76feb2fd36bfe1a3279d2c7aa561e2
ferer00 <155906971+ferer00@users.noreply.github.com>
2025-05-21 20:59:39 +0300

    Update Schema.java

--- src/main/java/org/example/Schema.java
+++ src/main/java/org/example/Schema.java
@@ -1,7 +1,15 @@
-// ================= Schema.java =================
-package org.example;
+package configlib;
 
+import java.util.Map;
 
 public class Schema {
+    private Map<String, Char[]> fields;
 
+    public Schema(Map<String, Char[]> fields) {
+        this.fields = fields;
+    }
+
+    public Map<String, Char[]> getFields() {
+        return fields;
+    }
 }
